6a8454791995f78c1904a8cd465e24ad7a9b19ba
Shreya-singh-uix <shreya@uixlabs.in>
2025-09-24 15:02:53 +0530

    middleware

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthGuard } from '@features/auth-guard';
 import { Toaster } from '@shared/ui/sonner';
 
 const queryClient = new QueryClient({});
@@ -9,10 +8,8 @@ const queryClient = new QueryClient({});
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthGuard>
         {children}
         <Toaster />
-      </AuthGuard>
     </QueryClientProvider>
   );
 }

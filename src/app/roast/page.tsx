56a8e559a996d5c71c63b408b64b73eaffb10d1f
Shreya-singh-uix <shreya@uixlabs.in>
2025-12-19 02:29:44 +0530

    roast-page

--- src/app/roast/page.tsx
+++ src/app/roast/page.tsx
@@ -13,6 +13,9 @@ import { RoastLoading } from "./components/roast-loading";
 import { TypewriterRoast } from "./components/typewriter-roast";
 
 import { trackEvent } from "@shared/lib/analytics/Mixpanel";
+import { RoastFirstSection } from "@widgets/roast/ui/roast-first-section";
+
+import { RoastRoastsSection } from "@widgets/roast/ui/roast-roasts-section";
 
 export default function RoastPage() {
   const [isDragging, setIsDragging] = useState(false);
@@ -51,7 +54,6 @@ export default function RoastPage() {
     },
   });
   const shouldHideOverflow = !response || isPending;
-  const isFireFixed = !response || isPending;
 
   const handleDragOver = (e: React.DragEvent) => {
     e.preventDefault();
@@ -101,41 +103,56 @@ export default function RoastPage() {
   return (
     <div
       className={cn(
-        "relative min-h-screen bg-white w-full font-sans overflow-x-hidden",
+        "relative min-h-screen bg-black w-full font-sans overflow-x-hidden",
         shouldHideOverflow && "overflow-hidden"
       )}
     >
       <div
-        className={cn("absolute inset-0", "[background-size:20px_20px]")}
+        className="fixed inset-0 z-[2] pointer-events-none"
         style={{
-          backgroundImage: "url('/images/RoastBackground.png')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
+          background:
+            "radial-gradient(circle at 0% 0%, #005FF2 0%, transparent 45%)",
         }}
       />
 
-      <Spotlight className="absolute inset-0" />
+      <RoastFirstSection />
+      <RoastRoastsSection />
+
+      {/* Flame Video */}
+      <div className="fixed inset-0 w-full h-full z-[1] pointer-events-none mix-blend-screen">
+        {/* Mobile Video */}
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-full object-cover object-bottom opacity-100 md:hidden"
+        >
+          <source src="/videos/flame.mp4" type="video/mp4" />
+        </video>
+
+        {/* Desktop Video */}
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="hidden md:block w-full h-full object-cover object-bottom opacity-100"
+        >
+          <source src="/videos/flame-desktop.mp4" type="video/mp4" />
+        </video>
+      </div>
 
       <main className="relative z-10 w-full min-h-screen flex flex-col">
         {/* <Header /> */}
-        <div className="flex flex-col items-center w-full pt-12 pb-8 md:pt-16 md:pb-12 lg:pt-8 lg:pb-4 px-5 flex-1 ">
+        <div className="flex flex-col items-center w-full pb-8 md:mt-10 md:pb-12 lg:pt-8 lg:pb-4 px-5 flex-1 ">
           <div className="flex flex-col items-center justify-between gap-18">
-            <div className="flex flex-col items-center justify-center ">
-              <Image
-                src={"/images/mobile-logo.png"}
-                alt="AI"
-                width={200}
-                height={70}
-                className="inline-block"
-              />
-            </div>
-            <div className="flex flex-col items-center justify-between gap-4 w-full md:w-1/2">
-              <h1 className="text-lg lg:text-[32px] font-normal leading-tight text-center text-white">
+            <div className="flex flex-col items-center justify-between gap-1 w-full md:w-1/2">
+              <h1 className="text-lg lg:text-2xl font-normal leading-tight text-center text-white md:w-120">
                 Your resume is holding you back.
               </h1>
 
-              <span className="text-4xl rounded-full text-white font-semibold shadow-lg lg:text-base py-1.5 lg:py-1 px-4 text-center">
+              <span className="text-5xl md:text-6xl rounded-full text-white font-semibold shadow-lg md:w-[500px] py-1.5 lg:py-1 px-4 text-center">
                 Let’s roast it. Then fix it.
               </span>
             </div>
@@ -153,40 +170,75 @@ export default function RoastPage() {
               />
             </div>
           ) : (
-            <div
-              className={cn(
-                "border-2 border-[#C66101] border-dashed rounded-[16px] px-6 py-8 lg:px-[32px] lg:py-[20px] shadow-[0px_10px_15px_-3px_rgba(0,0,0,0.1),0px_4px_6px_-4px_rgba(0,0,0,0.1)] flex flex-col items-center justify-center transition-colors cursor-pointer mt-6 lg:mt-3 w-full md:w-1/2",
-                "bg-gradient-to-br from-[#0D0600] to-[#B35802]",
-                isDragging ? "border-[#005ff2]" : "hover:opacity-95"
-              )}
-              onDragOver={handleDragOver}
-              onDragLeave={handleDragLeave}
-              onDrop={handleDrop}
-              onClick={onUploadClick}
-              onKeyDown={handleUploadKeyDown}
-              role="button"
-              tabIndex={0}
-            >
-              <input
-                type="file"
-                ref={fileInputRef}
-                onChange={handleFileSelect}
-                className="hidden"
-                accept=".pdf"
-              />
-              <h2 className="text-[#FFA855] text-[24px] lg:text-[16px] font-semibold mb-1.5 lg:mb-1">
-                Roast My Resume
-              </h2>
+            <div className="relative w-full md:w-1/2 mt-20 lg:mt-18 flex justify-center">
+              <div className="absolute -top-16 left-0 right-0 pointer-events-none">
+                {/* Left image */}
+                <Image
+                  src="/images/backimg.png"
+                  alt=""
+                  width={180}
+                  height={180}
+                  className="absolute -left-10 top-0 opacity-80 md:left-10 md:scale-125
+"
+                />
 
-              <div className=" w-12 h-12 lg:w-11 lg:h-11 rounded-full flex items-center justify-center mb-2 lg:mb-2">
-                <div className="relative w-6 h-6 lg:w-5 lg:h-5">
-                  <UploadIcon className="w-6 h-6 lg:w-5 lg:h-5 text-[#FFA855]" />
-                </div>
+                {/* Right image */}
+                <Image
+                  src="/images/backimg.png"
+                  alt=""
+                  width={180}
+                  height={180}
+                  className="absolute -right-10 top-0 opacity-80 rotate-[22deg] md:right-10 md:scale-125
+"
+                />
               </div>
 
-              <p className="text-[#FFA855] text-sm mb-3 lg:mb-2">
-                Select File from device
-              </p>
+              <div
+                className={cn(
+                  "relative border-2 border-dashed z-10 border-[#C66101] border-b-0 rounded-[16px] px-6 pt-20 py-6 lg:px-11 lg:py-11 shadow-[0px_10px_15px_-3px_rgba(0,0,0,0.1),0px_4px_6px_-4px_rgba(0,0,0,0.1)] flex flex-col items-center justify-center transition-colors cursor-pointer mt-6 lg:mt-3 w-full md:w-1/2",
+                  isDragging ? "border-[#005ff2]" : "hover:opacity-95"
+                )}
+                style={{
+                  background:
+                    "linear-gradient(180deg, #0D0600 0%, #B35802 100%)",
+                }}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+                onClick={onUploadClick}
+                onKeyDown={handleUploadKeyDown}
+                role="button"
+                tabIndex={0}
+              >
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  onChange={handleFileSelect}
+                  className="hidden"
+                  accept=".pdf"
+                />
+                <h2 className="text-[#FFA855] text-2xl lg:text-[30px] font-semibold mb-1.5 lg:mb-1">
+                  Roast My Resume
+                </h2>
+
+                <div className=" w-12 h-12 lg:w-11 lg:h-11 rounded-full flex items-center justify-center mb-2 lg:mb-2">
+                  <div className="relative w-11 h-11">
+                    <Image
+                      src="/images/upload-cloud.svg"
+                      alt="Upload"
+                      fill
+                      className="object-contain"
+                    />{" "}
+                  </div>
+                </div>
+
+                <p className="text-[#FFA855] text-sm mb-3 lg:mb-2 lg:text-lg">
+                  Select File from device
+                </p>
+                <p className="text-[#FFA855]/70 text-xs mt-1">
+                  JPEG and PDF formats, up to 5MB
+                </p>
+              </div>
             </div>
           )}
         </div>
---
+++ src/widgets/roast/ui/header-section.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import Header from "@widgets/landing-page/ui/header-section";
+
+export const HeaderSection = () => {
+  return <Header variant="roast" />;
+};

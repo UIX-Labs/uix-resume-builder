0d5d08885d8bee3eb23306c8898bd45a1dbae2f0
Enzo Uix <enzo@uixlabs.in>
2026-01-07 13:06:23 +0530

    fix:build issue

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,15 @@
 import AuthPageWidget from '@widgets/auth-page';
 import Image from 'next/image';
+import { Suspense } from 'react';
 
 export default function AuthPage() {
   return (
     <div className="w-full h-full">
       <Image src="images/landing-page-bg.svg" alt="Background" fill className="object-cover -z-10" priority />
       <div>
-        <AuthPageWidget />
+        <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Loading...</div>}>
+          <AuthPageWidget />
+        </Suspense>
       </div>
     </div>
   );

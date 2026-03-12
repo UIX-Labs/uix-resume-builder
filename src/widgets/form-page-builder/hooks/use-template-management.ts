43cc680fac7321317d189e95e853a7db4037f22c
ellaire-uix <mohsina@uixlabs.in>
2026-03-12 14:17:22 +0530

    undo of useEffect in use template

--- src/widgets/form-page-builder/hooks/use-template-management.ts
+++ src/widgets/form-page-builder/hooks/use-template-management.ts
@@ -1,9 +1,8 @@
 import { useUpdateResumeTemplate } from '@entities/resume';
 import type { Template } from '@entities/template-page/api/template-data';
-import MohsinaTemplate1 from '@features/resume/templates/mohsina-template-1';
 import aniketTemplate from '@features/resume/templates/standard';
 import { trackEvent } from '@shared/lib/analytics/Mixpanel';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { toast } from 'sonner';
 interface UseTemplateManagementParams {
   resumeId: string;
@@ -17,12 +16,12 @@ export function useTemplateManagement({ resumeId, initialTemplate, initialTempla
 
   const { mutateAsync: updateResumeTemplateMutation } = useUpdateResumeTemplate();
 
-    useEffect(() => {
-    if (MohsinaTemplate1) {  
-     setSelectedTemplate(MohsinaTemplate1);
-   }
- }, [MohsinaTemplate1]);
-      
+  //useEffect(() => {
+  // if (initialTemplate) {
+  //    setSelectedTemplate(initialTemplate);
+  //   }
+  // }, [initialTemplate]);
+
   // useEffect(() => {        /   if (initialTemplateId) {
   //     setSelectedTemplateId(initialTemplateId);
   //   }
